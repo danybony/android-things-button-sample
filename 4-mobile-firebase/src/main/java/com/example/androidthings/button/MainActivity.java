aef256fec542bf4cc444d87b8c693ec0a75d1449
Daniele Bonaldo <dany.bony@gmail.com>
2017-02-11 18:31:52 +0100

    Add Firebase sync logic to mobile app

--- 4-mobile-firebase/src/main/java/com/example/androidthings/button/MainActivity.java
+++ 4-mobile-firebase/src/main/java/com/example/androidthings/button/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.androidthings.button;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseCounter firebaseCounter;
+
+    private TextView counterValue;
+    private View negativeLight;
+    private View positiveLight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseCounter = FirebaseCounter.newInstance();
+        counterValue = (TextView) findViewById(R.id.counter_value);
+        negativeLight = findViewById(R.id.light_negative);
+        positiveLight = findViewById(R.id.light_positive);
+
+        TextView decreaseButton = (TextView) findViewById(R.id.button_decrease);
+        decreaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseCounter.decreaseCounter();
+            }
+        });
+
+        TextView increaseButton = (TextView) findViewById(R.id.button_increase);
+        increaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseCounter.increaseCounter();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseCounter.setListener(counterListener);
+    }
+
+    private FirebaseCounter.Listener counterListener = new FirebaseCounter.Listener() {
+        @Override
+        public void onCounterUpdated(long value) {
+            displayStatus(value);
+        }
+    };
+
+    private void displayStatus(long counter) {
+        counterValue.setText(String.valueOf(counter));
+        if (counter == 0) {
+            negativeLight.setSelected(false);
+            positiveLight.setSelected(false);
+        } else if (counter < 0) {
+            negativeLight.setSelected(true);
+            positiveLight.setSelected(false);
+        } else {
+            negativeLight.setSelected(false);
+            positiveLight.setSelected(true);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        firebaseCounter.removeListener();
+        super.onPause();
     }
 }
